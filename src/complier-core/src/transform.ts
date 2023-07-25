22e6accb5e64549169bae62ab5d8e020ca5a2730
edisonCheng <17365253013@163.com>
2023-07-25 22:08:24 +0800

    transform模块实现

---
+++ src/complier-core/src/transform.ts
@@ -0,0 +1,25 @@
+export function transform(ast, options) {
+  const context = createTransformContext(ast, options);
+  traverseNode(ast, context);
+}
+
+function createTransformContext(ast: any, options: any) {
+  return {
+    ast,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+}
+
+//深度优先遍历ast(抽象语法树)
+function traverseNode(node, context: any) {
+  const { nodeTransforms } = context;
+  for (const plugin of nodeTransforms) {
+    plugin(node);
+  }
+  const { children } = node;
+  if (children) {
+    for (const childNode of children) {
+      traverseNode(childNode, context);
+    }
+  }
+}
